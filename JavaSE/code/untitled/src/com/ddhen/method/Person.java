6a5bd32a6b12f0a4c5f8111f15a16902f51a0ac6
Ptolemye <100210260+Ptolemye@users.noreply.github.com>
2024-04-29 22:32:21 +0800

    Add files via upload

---
+++ JavaSE/code/untitled/src/com/ddhen/method/Person.java
@@ -0,0 +1,11 @@
+package com.ddhen.method;
+
+public class Person {
+    protected int age=3;
+    public void a(){
+        System.out.println(1);
+    }
+    public void b(){
+
+    }
+}
---
+++ JavaSE/code/untitled/src/com/ddhen/method/test.java
@@ -0,0 +1,11 @@
+package com.ddhen.method;
+
+import com.ddhen.method.Demo2.Student;
+
+public class test {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+    }
+
+}
